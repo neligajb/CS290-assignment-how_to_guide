08ddcffb35fda6fd5dc6876f7f1c15ac02d0bb91
Boone N <boone.neligan@gmail.com>
2015-05-21 19:36:14 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <LINK href="style.css" rel="stylesheet" type="text/css">
+  <title>Boone's Video Repository</title>
+</head>
+<?php
+include "content.php";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+<body>
+  <header>
+    <
+  </header>
+</body>
+</html>
